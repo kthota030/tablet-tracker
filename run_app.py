451139e101796764dd3a32c05be8b9673c828355
kthota030 <thotak124@gmail.com>
2026-07-07 13:53:36 -0500

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -5,4 +5,4 @@ from streamlit.web import cli as stcli
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
     sys.argv = ["streamlit", "run", "app.py", "--server.port=8501", "--global.developmentMode=false"]
-    stcli.main(
+    stcli.main()
